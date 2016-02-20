35a18f46ea4e9e2d0d2b06b995a446caca32a351
Balint  Rikker <balint.rikker14@imperial.ac.uk>
2016-02-20 22:48:44 +0000

    only inject on subreddit frontpages

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,5 +1,11 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.executeScript(tab.ib, {
+var getLocation = function(href) {
+  var l = document.createElement("a");
+  l.href = href;
+  return l;
+};
+
+chrome.tabs.query({'active': true}, function(tab) {
+  chrome.tabs.executeScript(null, {
     file: 'content.js'
   });
 });
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,11 +1,14 @@
-// Content injection, called by backround.js
-alert("Hello, im working");
-//  create element to be inserted
-var node = document.createElement('div');
-node.style.position = 'fixed';
-node.style.top = 0;
-node.style.left = 0;
-node.textContent = 'Injection!';
+// only inject on subreddit pages
+var pos = document.location.pathname;
 
-document.body.appendChild(node);
+if (/^(\/|)r\/[a-zA-Z0-9]*(\/|)$/.test(pos)) {
+  //  create element to be inserted
+  var node = document.createElement('div');
+  node.style.position = 'fixed';
+  node.style.top = 0;
+  node.style.left = 0;
+  node.textContent = 'Injection!';
+  
+  document.body.appendChild(node);
+}
 
