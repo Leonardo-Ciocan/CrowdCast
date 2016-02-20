5a5ffea5d093e6da91af092fb2b23d6a1a624303
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2016-02-20 16:34:55 +0000

    UI fixes

--- website-frontend/src/index.jsx
+++ website-frontend/src/index.jsx
@@ -22,15 +22,18 @@ var Header = React.createClass({
             left:"50%",
             top:"0px",
             bottom:"0px",
-            width:"200px",
-            marginLeft:"-100px",
-            background:"rgba(0,0,0,0.15)"
+            width:"350px",
+            marginLeft:"-175px",
+            background:"rgba(0,0,0,0.06)",
+            borderLeft:"1px solid lightgray",
+            borderRight:"1px solid lightgray"
         };
 
         return (
             <div style={style}>
                 <div style={centerStyle}>
-
+                    <h1 style={{textAlign:"center", fontFamily:"Helvetica" , fontWeight:"200", fontSize:"12pt",margin:"10px" ,marginBottom:"5px"}}>Current episode</h1>
+                    <h3 style={{textAlign:"center", fontFamily:"Helvetica" , fontWeight:"200", fontSize:"9pt",marginLeft:"10px" ,marginTop:"0px"}}>05:00 / 12:33</h3>
                 </div>
             </div>
         );
@@ -43,16 +46,22 @@ var Episode = React.createClass({
 
         var style = {
             width:"100%",
+            height:"45px",
             background:"#fafafa",
             border:"1px solid lightgray",
             borderRadius:"5px",
-            marginTop:"10px"
+            marginTop:"10px",
+            position:"relative"
         };
 
         return (
             <div style={style}>
-                <h1 style={{fontFamily:"Helvetica" , fontWeight:"200", fontSize:"12pt",margin:"10px" ,marginBottom:"5px"}}>Podcast episode title</h1>
-                <h3 style={{fontFamily:"Helvetica" , fontWeight:"200", fontSize:"9pt",marginLeft:"10px" ,marginTop:"0px"}}>Episode 1 | 12:33</h3>
+
+                <h1 style={{display:"inline-block",lineHeight:"45px",margin:"0px",verticalAlign:"middle",fontFamily:"Helvetica" , fontWeight:"200", fontSize:"12pt",marginLeft:"100px"}}>Podcast episode title</h1>
+                <h1 style={{display:"inline-block",position:"absolute",right:"0px",lineHeight:"45px",margin:"0px",verticalAlign:"middle",fontFamily:"Helvetica" , fontWeight:"200", fontSize:"12pt",marginRight:"10px"}}>▶</h1>
+                <h3 style={{width:"80px",borderRight:"1px solid lightgray",display:"inline-block",lineHeight:"22px",margin:"0px",verticalAlign:"middle",position:"absolute" , top:"0px",left:"0px" , bottom:"50%",fontFamily:"Helvetica" , fontWeight:"200", fontSize:"9pt",marginLeft:"10px" ,marginTop:"0px"}}>Episode 1</h3>
+                <h3 style={{width:"80px",borderRight:"1px solid lightgray",display:"inline-block",lineHeight:"22px",margin:"0px",verticalAlign:"middle",position:"absolute" , top:"50%",left:"0px" , bottom:"0px",fontFamily:"Helvetica" , fontWeight:"200", fontSize:"9pt",marginLeft:"10px" ,marginTop:"0px"}}>12:33</h3>
+
             </div>
         );
     }
