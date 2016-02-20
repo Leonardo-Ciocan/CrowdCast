c6440b4aa48408c0de0941f102697798d6b43d10
Benedikt Lorch <benedikt.lorch@fau.de>
2016-02-20 18:25:05 +0000

    Setup bash script for database

---
+++ server/setup.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+set -x
+
+## Set up database: Create databases and user ##
+USER=root
+PASS=dbpass
+mysql -u $USER -p$PASS << EOF
+
+CREATE DATABASE IF NOT EXISTS crowdcast;
+GRANT USAGE ON *.* TO 'crowdcast'@'%' IDENTIFIED BY 'mlh';
+GRANT ALL PRIVILEGES ON crowdcast.* TO 'crowdcast'@'%';
+FLUSH PRIVILEGES;
+
+EOF
+
+## Run server ##
+# gradle run
