d5afbc4154f35e12ffd494a4b6e0394c56540bc7
Balint  Rikker <balint.rikker14@imperial.ac.uk>
2016-02-20 21:28:56 +0000

    fix injection bug

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,3 +1,11 @@
 // Content injection, called by backround.js
 alert("Hello, im working");
+//  create element to be inserted
+var node = document.createElement('div');
+node.style.position = 'fixed';
+node.style.top = 0;
+node.style.left = 0;
+node.textContent = 'Injection!';
+
+document.body.appendChild(node);
 
