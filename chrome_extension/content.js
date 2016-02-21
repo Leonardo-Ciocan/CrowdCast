0e0225c563ce63251e15c7179c41d372335dd45c
Balint  Rikker <balint.rikker14@imperial.ac.uk>
2016-02-21 09:32:34 +0000

    Client links on sub header

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -17,19 +17,26 @@ function loadScript(url, callback) {
 function existsAtServer(relurl, parentelem) {
   // transform relative url to filtered fullurl
   var fullurl = "www.reddit.com" + relurl;
-  var filtered = fullurl.replace('/', '');
-  filtered = filtered.replace(':', '');
+  var filtered = fullurl.replace(/\//g, '');
+  filtered = filtered.replace(/:/g, '');
+  filtered = filtered.replace(/\?/g, '');
   filtered = "/episode/" + filtered;
   // make ajax query to server.
-  $.ajax({url: 'http://52.49.190.175', data: filtered, success: function(result) {
-  annotateElem(result, parentelem);
-   }});
+  $.ajax({
+    url: 'http://localhost:8080',
+        data: filtered, 
+        dataType: 'json',
+        success: function(result) {
+                   annotateElem(result.websiteUrl, parentelem);
+                 }
+  });
 }
 
 function annotateElem(sitelink, parentelem) {
+  alert("annotated! : " + sitelink);
   jQuery('<a/>', {
         id: 'foo',
-        href: sitelink,
+        href: 'http://localhost:8080/episodes' + sitelink,
         title: 'happy easter',
         rel: 'external',
         text: 'Play on CrowdCast'
@@ -37,32 +44,49 @@ function annotateElem(sitelink, parentelem) {
 }
 
 function main() {
-  // only inject on subreddit pages
   var pos = document.location.pathname;
+  // inject on subreddit pages
+  if (/^(\/|)r\/[a-zA-Z0-9]*(\/top|\/new|)(\/|)*$/.test(pos)) {
+    var re = /^\/r\/([a-zA-Z0-9]*)(\/|).*$/;
+    var subname = pos.match(re)[1];
     
-  if (/^(\/|)r\/[a-zA-Z0-9]*(\/|)$/.test(pos)) {
-    // jquery all title elements
-    var titles = $( 'p' ).filter( '.title' );
 
-    // get href from each 'a' element in class.
-    titles.each(function() {
-      var children = $(this).children();
-      var links = children.filter( 'a' );
-      var url = links.attr("href");
-      existsAtServer(url, this);
-      return true;
+    // inject link on subreddit frontpage
+    var clientUrl = "http://example.com/";
+    var imageUrl  = "https://lh3.ggpht.com/OOXV4V9YyovafA10xZhq0QgWNwFwCEhMI9kWJ2FDkjMmLa7rDWJmSmnsgOtMWdDGg3A=w300"
+
+    var par = $( 'div' ).filter(function() {
+      return $(this).attr('id') == 'header-bottom-left';
     });
 
-    // create element to be inserted
-    var node = document.createElement('div');
-    node.style.position = 'fixed';
-    node.style.top = 0;
-    node.style.left = 0;
-    node.textContent = 'Injection!';
+    jQuery('<a/>', {
+          id: 'foo',
+          href: clientUrl + "?r=" + subname,
+          title: 'happy easter',
+          rel: 'external',
+          text: 'Play on CrowdCast'
+    }).appendTo($(par));
     
-    document.body.appendChild(node);
-  }
-}
+    
+    
 
+    // // jquery all title elements
+    // var titles = $( 'p' ).filter( '.title' );
+    // 
+    // // get href from each 'a' element in class.
+    // titles.each(function() {
+    //   var children = $(this).children();
+    //   var links = children.filter( 'a' );
+    //   var url = links.attr("href");
+    //   existsAtServer(url, this);
+    //   return true;
+    // });
+  }
+  
+  // inject in comment thread
+  if (/^(\/|)r\/[a-zA-Z0-9]*\/comments\/.*/.test(pos)) {
+  }
+
+}
 
 loadScript('https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js', main);
